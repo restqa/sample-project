e766776c3027f0a4194829bf010f0c689ddc3276
Olivier <rodomond.o@gmail.com>
2020-02-25 16:30:39 +0700

    add new microservice

---
+++ 2-microservice-users/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const NodeCache = require("node-cache")
+const { v4:uuidv4 } =  require('uuid')
+
+const Cache = new NodeCache()
+let { PORT } = process.env
+PORT = PORT || 8080
+
+express()
+  .use(require('body-parser').json())
+  .use('/users/:id', (req, res, next) => {
+    let err
+    req.user = Cache.get(req.params.id)
+    if (!req.user) {
+      err = {code: 404, msg: `user not found`}
+    }
+    next(err)
+  })
+  .post('/users', (req, res, next) => {
+    let { body } = req
+    body = {
+      id: uuidv4(),
+      ...body
+    }
+
+    if (!body.firstname) return next({code: 406, msg: 'firstname is mandatory'})
+    if (!body.lastname) return next({code: 406, msg: 'lastname is mandatory'})
+
+    success = Cache.set(body.id, body)
+    res.json(body)
+  })
+  .get('/users', (req, res) => {
+    let mykeys = Cache.keys()
+    res.json(Object.values(Cache.mget(mykeys)))
+  })
+  .get('/users/:id', (req, res, next) => {
+    res.json(req.user)
+  })
+  .put('/users/:id', (req, res, next) => {
+    let { body } = req
+    body = Object.assign(req.user, body)
+
+    if (!body.firstname) return next({code: 406, msg: 'firstname is mandatory'})
+    if (!body.lastname) return next({code: 406, msg: 'lastname is mandatory'})
+
+    Cache.set(body.id, body)
+    res.json(body)
+  })
+  .delete('/users/:id', (req, res, next) => {
+    Cache.del(req.user.id)
+    res.sendStatus(204)
+  })
+  .use((err, req, res, next) => {
+    res.json(err)
+  })
+  .listen(PORT, () => console.log('start server on ' + PORT))
